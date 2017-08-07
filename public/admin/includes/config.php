37166f5956f4d7c6b137d5ba285a583712d9e270
Sam Harvey <s.rharveyiv@gmail.com>
2017-08-06 22:03:46 -0700

    project set up
    initial project set up/commit

---
+++ public/admin/includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database Connection Constants
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'scotchbox');
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection) {
+  echo "Connected";
+}
+
+ ?>
